7133ba38e0cfd59a74040daf626b72fae23a9152
Froggy-Lord <279880356+Froggy-Lord@users.noreply.github.com>
2026-08-02 15:46:07 +1000

    config: fix auriga config [skip ci]

--- src/main/java/com/froggylord/constellation/config/AurigaConfig.java
+++ src/main/java/com/froggylord/constellation/config/AurigaConfig.java
@@ -1,33 +1,6 @@
 package com.froggylord.constellation.config;
 
 public class AurigaConfig extends BaseConfigGroup {
+    { enabled = false; }
     @Override public int currentVersion() { return 0; }
-    public boolean experimentHelper = true;
-
-    
-    public boolean ultrasequencerSolver = true;
-    public boolean superpairsSolver = true;
-    public boolean bingoHelper = true;
-    public boolean shCalcCommand = true; 
-    public boolean anvilHelper = true;    
-    public boolean powerStoneDisplay = true; 
-    public boolean clockReminder = true;     
-    public boolean essenceShopHelper = true; 
-    public boolean reforgeHelper = true;     
-    public boolean chocolateFactoryHelper = true; 
-    public boolean minionHopperTracker = true;   
-    public boolean evolvingItemTimer = true;       
-    public boolean attributeShardHelper = true;    
-    public boolean enchantTableHelper = true;       // show cheapest enchant path (cm...
-    public boolean brewHelper = true;              
-    public boolean godPotDisplay = true;            
-    public boolean teleportPadHelper = true;        
-    public boolean pathfindUtil = true;             
-    public boolean cosmeticHelper = true;
-    public boolean hexHelper = true;
-    public boolean ultimateEnchantDisplay = true;
-    public boolean dungeonJournalHelper = true;
-    public boolean carnivalUpgradeHelper = true;
-    public boolean nucleusBundleHelper = true;
-    public boolean crystalHollowsShortcut = true;
 }
