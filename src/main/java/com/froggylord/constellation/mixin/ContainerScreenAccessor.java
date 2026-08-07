df1342fd01591e1e75d0b6f1099b11a7bac276e7
Froggy-Lord <279880356+Froggy-Lord@users.noreply.github.com>
2026-08-07 14:02:41 +1000

    mixin: clean up container screen accessor [skip ci]

--- src/main/java/com/froggylord/constellation/mixin/ContainerScreenAccessor.java
+++ src/main/java/com/froggylord/constellation/mixin/ContainerScreenAccessor.java
@@ -3,6 +3,7 @@ package com.froggylord.constellation.mixin;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import net.minecraft.world.inventory.Slot;
 
 @Mixin(AbstractContainerScreen.class)
 public interface ContainerScreenAccessor {
@@ -11,4 +12,13 @@ public interface ContainerScreenAccessor {
 
     @Accessor("topPos")
     int constellation$top();
+
+    @Accessor("imageWidth")
+    int constellation$imageWidth();
+
+    @Accessor("imageHeight")
+    int constellation$imageHeight();
+
+    @Accessor("hoveredSlot")
+    Slot constellation$hoveredSlot();
 }
